7a1d7b4dc5a631eb26fd5bc3c442dee2bd5d4a3b
Brian Seitz <brnstz@gmail.com>
2011-08-14 18:52:35 -0400

    - Printing out id3 tag

--- id3go.go
+++ id3go.go
@@ -4,33 +4,32 @@ import "flag"
 import "fmt"
 import "os"
 
-func readFile(filename string) {
-    var buff []byte
+func readFile(filename string) (string) {
+    buff := make([]byte, 128, 128)
     
-    f, err := os.Open(filename)
+    f, err := os.Open(filename, os.O_RDONLY, 0)
     if err != nil {
-        return err
+        return fmt.Sprintf("%v", err)
     }
     defer f.Close()
 
     // Read last 128 bytes of file to see ID3 tag
-    f.Seek(128, 2)
-    f.Read(buff, 128)
+    f.Seek(-128, 2)
+    f.Read(buff)
 
     fmt.Println(buff)
+
+    return ""
 }
 
 func main() {
-    var myint *string = flag.String("album", "Transformer", "album name")
+    //var myint *string = flag.String("album", "Transformer", "album name")
 
     flag.Parse()
 
     for _, filename := range(flag.Args()) {
-    
+        readFile(filename)
     }
-
-    fmt.Println(*myint)
-
 
     //fmt.Printf("Its value is: %v %v %v %v", *myint, flag.Args(), flag.NArg(), flag.NFlag())
 }
